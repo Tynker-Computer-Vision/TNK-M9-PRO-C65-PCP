984f2f85438d234830509fb23a25b65cee8f5f8b
vishalgddam873 <vishal18@navgurukul.org>
2023-06-06 12:43:07 +0530

    Code  updated

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ import cv2
 inputPath = 'static/img1.jpg'
 
 # Load the color image
-orignalImage = cv2.imread(inputPath)
+originalImage = cv2.imread(inputPath)
 
 # ------------Convert image to Grayscale --------------
 
 # Convert the color image to grayscale image
-grayscaleImage = cv2.cvtColor(orignalImage, cv2.COLOR_BGR2GRAY)
+grayscaleImage = cv2.cvtColor(originalImage, cv2.COLOR_BGR2GRAY)
 
 # # ----------------- Convert image to Sketch Image ---------------
 
